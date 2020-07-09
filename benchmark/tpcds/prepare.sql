752f2311a56cfae20d9bb584601b2a56eda5af83
mahjonp <junpeng.man@gmail.com>
2020-07-09 12:55:02 +0800

    add ssb and tpcds
    Signed-off-by: mahjonp <junpeng.man@gmail.com>

---
+++ benchmark/tpcds/prepare.sql
@@ -0,0 +1,8 @@
+analyze table lineitem;
+analyze table orders;
+analyze table part;
+analyze table partsupp;
+analyze table supplier;
+analyze table customer;
+analyze table region;
+analyze table nation;
