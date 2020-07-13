2d376412a65e8c23611462d9efae850a0d14c997
mahjonp <junpeng.man@gmail.com>
2020-07-13 17:56:11 +0800

    init commit
    Signed-off-by: mahjonp <junpeng.man@gmail.com>

---
+++ cmd/card.go
@@ -0,0 +1,54 @@
+// Copyright 2020 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/urfave/cli/v2"
+	"strings"
+
+	"github.com/chaos-mesh/horoscope/pkg/horoscope"
+)
+
+var (
+	cardinalitor *horoscope.Cardinalitor
+	columns      string
+	cardCommand  = &cli.Command{
+		Name:   "card",
+		Usage:  "test the cardinality estimations",
+		Action: testCard,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "columns",
+				Usage:       "collect cardinality estimation error, format of 't1:c1,t1:c2,t2:c1...'",
+				Destination: &columns,
+			},
+		},
+	}
+)
+
+func testCard(*cli.Context) error {
+	tableColumns := make(map[string][]string)
+	for _, pair := range strings.Split(columns, ",") {
+		values := strings.Split(pair, ":")
+		table := values[0]
+		column := values[1]
+		if _, e := tableColumns[table]; !e {
+			tableColumns[table] = make([]string, 0)
+		}
+		tableColumns[table] = append(tableColumns[table], column)
+	}
+	cardinalitor = horoscope.NewCardinalitor(Exec, 0, tableColumns)
+	_, err := cardinalitor.Test()
+	return err
+}
---
+++ pkg/horoscope/card.go
@@ -0,0 +1,93 @@
+package horoscope
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/chaos-mesh/horoscope/pkg/executor"
+	log "github.com/sirupsen/logrus"
+)
+
+type CardinalityQueryType int
+
+const (
+	TypeEMQ CardinalityQueryType = iota
+	TypeDCT
+	TypeRGE
+)
+
+type Cardinalitor struct {
+	exec         executor.Executor
+	Type         CardinalityQueryType
+	TableColumns map[string][]string
+}
+
+func NewCardinalitor(exec executor.Executor, typ CardinalityQueryType, tableColumns map[string][]string) *Cardinalitor {
+	return &Cardinalitor{
+		exec:         exec,
+		Type:         typ,
+		TableColumns: tableColumns,
+	}
+}
+
+func (c *Cardinalitor) Test() (map[string]map[string]*Metrics, error) {
+	result := make(map[string]map[string]*Metrics)
+	var fun func(tableName, columnName string) (*Metrics, error)
+	switch c.Type {
+	case TypeEMQ:
+		fun = c.testEMQ
+	default:
+		panic("implement me!")
+	}
+	for tableName, columns := range c.TableColumns {
+		if _, e := result[tableName]; !e {
+			result[tableName] = make(map[string]*Metrics)
+		}
+		tableMap := result[tableName]
+		for _, columnName := range columns {
+			m, err := fun(tableName, columnName)
+			if err != nil {
+				return nil, err
+			}
+			tableMap[columnName] = m
+		}
+	}
+	return result, nil
+}
+
+func (c *Cardinalitor) testEMQ(tableName, columnName string) (*Metrics, error) {
+	const batchSize = 1000
+	rows, err := c.exec.Query(fmt.Sprintf("SELECT COUNT(DISTINCT %s) FROM %s", columnName, tableName))
+	if err != nil {
+		return nil, fmt.Errorf("fetch count(distinct %s) from %s occurred an error: %v", columnName, tableName, err)
+	}
+	count, err := strconv.ParseInt(rows.Data[0][0], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	metrics := &Metrics{}
+	for i := 0; i*batchSize < int(count); i++ {
+		rows, err := c.exec.Query(fmt.Sprintf("SELECT DISTINCT %s FROM %s ORDER BY %s LIMIT %d, %d",
+			columnName, tableName, columnName, i*batchSize, batchSize,
+		))
+		if err != nil {
+			return nil, err
+		}
+		for _, value := range rows.Data {
+			ei, err := c.exec.ExplainAnalyze(fmt.Sprintf("SELECT %s FROM %s WHERE %s = '%s'", columnName, tableName, columnName, value))
+			if err != nil {
+				return nil, err
+			}
+			cis := executor.CollectEstAndActRows(ei)
+			qError := cis[0].QError
+			metrics.Values = append(metrics.Values, qError)
+			log.WithFields(log.Fields{
+				"table":    tableName,
+				"column":   columnName,
+				"value":    value,
+				"template": "EMQ",
+			}).Infof("q-error is %.1f", qError)
+		}
+	}
+	return metrics, nil
+}
